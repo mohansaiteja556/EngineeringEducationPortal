ab0f0ed06aa51cdf6ed3511ef2ead45d9f637d29
mohansaiteja556 <mohansaiteja556@gmail.com>
2018-11-26 10:33:05 +0530

    New Proect EEP Added to Github

---
+++ BusinessObjects/Dept.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects
+{
+    public class Dept
+    {
+        public string DeptId { get; set; }
+        public string DeptName { get; set; }
+        public string DeptRoll { get; set; }
+    }
+}
